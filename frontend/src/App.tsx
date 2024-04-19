7404178f38153ab0fdbd06288b60b0bd32174dbd
Tikhon Izmailov <tikhonbright@gmail.com>
2024-04-19 18:45:07 +0700

    [add] upload images

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
-import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { useCallback, useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
   const [cats, setCats] = useState<any[]>([])
+  const [img, setImg] = useState<any>(null)
+  const [nft, setNft] = useState<any>(null)
 
   useEffect(() => {
     fetch(import.meta.env.VITE_API_URL + 'api/cats')
@@ -14,11 +17,37 @@ function App() {
       })
   }, [])
 
+  const sendFile = useCallback(async () => {
+    try {
+      const data = new FormData()
+      data.append('nft', img)
+
+      await axios
+        .post(import.meta.env.VITE_API_URL + 'api/upload', data, {
+          headers: {
+            'content-type': 'multipart/form-data',
+          },
+        })
+        .then((res) => setNft(res.data.path))
+    } catch (error) {}
+  }, [img])
+
   return (
     <>
+      <img
+        style={{ width: '100px', height: '100px' }}
+        src={
+          import.meta.env.VITE_API_URL + 'images/nft-1713526772398-pirat.jpg'
+        }
+        alt=""
+      />
       {cats.map((cat) => (
         <h2 key={cat._id}>{cat?.name}</h2>
       ))}
+      <img src={import.meta.env.VITE_API_URL + nft} alt="nft" />
+
+      <input type="file" onChange={(evt) => setImg(evt.target.files[0])} />
+      <button onClick={sendFile}>Загрузить NFT</button>
     </>
   )
 }
